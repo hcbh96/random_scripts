6d89d92f23a0d312f49c27eaae838acdf2152890
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2023-02-03 17:13:12 +0000

    watermark image

---
+++ 23_02_03-hh-watermark_image.py
@@ -0,0 +1,59 @@
+# Script to add watermark to images in a folder
+# Usage: python3 watermark.py <folder> <watermark.png>
+# Example: python3 watermark.py images watermark.png
+
+import os
+import sys
+from PIL import Image
+
+def watermark_image(input_image_path, output_image_path, watermark_image_path, position):
+    try:
+        # Open the watermark image
+        watermark = Image.open(watermark_image_path)
+        # Open the input image
+        image = Image.open(input_image_path)
+        # Get the size of the watermark
+        width, height = watermark.size
+        # Get the size of the input image
+        image_width, image_height = image.size
+
+        # Calculate the position of the watermark
+        if position == 'top-left':
+            position = (0, 0)
+        elif position == 'top-right':
+            position = (image_width - width, 0)
+        elif position == 'bottom-left':
+            position = (0, image_height - height)
+        elif position == 'bottom-right':
+            position = (image_width - width, image_height - height)
+        else:
+            raise ValueError('Unknown position')
+        # Add the watermark to the image
+        image.paste(watermark, position, mask=watermark)
+        # Save the image
+        image.save(output_image_path)
+
+        print('Watermarked image saved as', output_image_path)
+    except Exception as e:
+        print(e)
+
+def main():
+    # Get the folder path
+    folder = sys.argv[1]
+    # Get the watermark path
+    watermark = sys.argv[2]
+    # Get the position of the watermark
+    position = sys.argv[3]
+    # Loop through the images in the folder
+    for filename in os.listdir(folder):
+        # Get the path to the image
+        input_image_path = os.path.join(folder, filename)
+        # Get the path to the output image
+        output_image_path = os.path.join(folder, 'watermarked-' + filename)
+        # Watermark the image
+        watermark_image(input_image_path, output_image_path, watermark, position)
+
+if __name__ == '__main__':
+    main()
+
+
